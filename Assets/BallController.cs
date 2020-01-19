775a3ab6445516a2408c993e95fe12db62776921
Go3786 <3786tsumori@gmail.com>
2020-01-19 22:45:34 +0900

    PinBall 課題
    GameOver時、Scoreを表示

---
+++ Assets/BallController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+	public GameObject ScoreText;
+	public int score=0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+		this.ScoreText = GameObject.Find ("ScoreText");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+			string scoretext = score.ToString ();
+			this.ScoreText.GetComponent<Text> ().text = "あなたのスコア:"+scoretext;
+		}
+	}
+	void OnCollisionEnter (Collision collision)
+	{
+		string yourtag = collision.gameObject.tag;
+		if (yourtag == " LargeCloudTag") {
+			score += 30;
+		} else if (yourtag == "SmallCloudTag") {
+			score += 20;
+		} else if (yourtag == "LargeStarTag") {
+			score += 15;
+		} else if (yourtag == "SmallStarTag") {
+			score += 5;
+		}
+	}
+
+}
+
+
+
+
+
+
+			
